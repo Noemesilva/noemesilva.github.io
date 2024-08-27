edebd462b30bd5194450e9610cb6e3c6b3599886
Noeme Silva <noeme16@gmail.com>
2024-08-27 12:21:33 -0300

    feat:  Adicionar funcionalidade para salvar tema no localStorage

--- README.md
+++ README.md
@@ -1,1 +1,83 @@
 # noemesilva.github.io
+
+🌟 Portfólio de Noeme Silva 🌟
+
+🎨 Sobre o Projeto
+
+Bem-vindo ao meu espaço virtual! Este é o portfólio pessoal de Noeme Silva, uma Desenvolvedora Full Stack apaixonada por Front-End. 
+Aqui você encontrará um vislumbre das minhas habilidades técnicas, projetos inovadores, minha jornada acadêmica e profissional, além de formas para entrar em contato comigo.
+
+🚀 Recursos Incríveis
+
+🌐 Design Responsivo: Experiência otimizada em qualquer dispositivo.
+
+🌈 Modo Claro/Escuro: Personalize seu visual com apenas um clique.
+
+📂 Seções Atraentes: Apresentação dos meus projetos, habilidades e mais!
+
+🔧 Tecnologias Utilizadas
+HTML5: Estrutura sólida e semântica.
+CSS3: Estilo elegante e moderno.
+JavaScript (ES6+): Funcionalidades dinâmicas e interativas.
+
+
+🛠️ Pré-requisitos
+Um navegador web moderno para uma visualização impecável.
+Um servidor local (recomendado para o desenvolvimento).
+
+🚀 Como Começar
+Clone o Repositório:
+
+bash
+Copiar código
+git clone https://github.com/Noemesilva/portfolio.git
+Entre no Diretório do Projeto:
+
+bash
+Copiar código
+cd portfolio
+Abra o index.html no Seu Navegador ou use um servidor local:
+
+Para um servidor local com Python, execute:
+bash
+Copiar código
+python -m http.server
+Então, acesse http://localhost:8000 no seu navegador.
+
+📂 Estrutura do Projeto
+css
+Copiar código
+portfolio/
+│
+├── index.html
+├── styles.css
+├── scripts.js
+├── content.json
+│
+├── assets/
+│   └── images/
+│       ├── logoBranca.png
+│       ├── Noeme.png
+│       └── ...
+│
+└── README.md
+
+🎨 Personalização
+Quer deixar o site com a sua cara? Siga estes passos:
+
+Abra o arquivo content.json.
+Atualize as informações que desejar.
+Salve e recarregue a página para ver as mudanças em tempo real!
+
+🤝 Contribuição
+Adoraria receber sua contribuição! Abra uma issue ou envie um pull request para ajudar a aprimorar este projeto.
+
+
+📬 Contato
+
+Noeme Silva: noeme16@gmail.com
+GitHub: https://github.com/Noemesilva/portfolio
+Live Demo: https://noemesilva.github.io/portfolio
+
+© 2024 Noeme Silva. Todos os direitos reservados.
+
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,31 +1,62 @@
 const toggleTheme = document.getElementById("toggleTheme");
-const rootHtml = document.documentElement
+const rootHtml = document.documentElement;
 const accordionHeaders = document.querySelectorAll(".accordion__header");
 const menuLinks = document.querySelectorAll(".menu__link");
 
-function changeTheme(){
+function changeTheme() {
   const currentTheme = rootHtml.getAttribute("data-theme");
 
-  currentTheme === "dark" ? rootHtml.setAttribute("data-theme", "light") : rootHtml.setAttribute("data-theme", "dark")
+  currentTheme === "dark"
+    ? rootHtml.setAttribute("data-theme", "light")
+    : rootHtml.setAttribute("data-theme", "dark");
 
-  toggleTheme.classList.toggle("bi-sun")
-  toggleTheme.classList.toggle("bi-moon-stars")
+  toggleTheme.classList.toggle("bi-moon-stars");
+  toggleTheme.classList.toggle("bi-sun");
 }
 
 toggleTheme.addEventListener("click", changeTheme);
 
-accordionHeaders.forEach(header => {
+accordionHeaders.forEach((header) => {
   header.addEventListener("click", () => {
     const accordionItem = header.parentElement;
     const accordionActive = accordionItem.classList.contains("active");
 
-    accordionActive ? accordionItem.classList.remove("active") : accordionItem.classList.add("active");
-  })
-})
+    accordionActive
+      ? accordionItem.classList.remove("active")
+      : accordionItem.classList.add("active");
+  });
+});
 
-menuLinks.forEach(item => {
+menuLinks.forEach((item) => {
   item.addEventListener("click", () => {
-    menuLinks.forEach(i => i.classList.remove("active"));
+    menuLinks.forEach((i) => i.classList.remove("active"));
     item.classList.add("active");
-  })
-})
+  });
+});
+
+function changeTheme() {
+  const currentTheme = rootHtml.getAttribute("data-theme");
+
+  const newTheme = currentTheme === "dark" ? "light" : "dark";
+  rootHtml.setAttribute("data-theme", newTheme);
+
+  toggleTheme.classList.toggle("bi-sun");
+  toggleTheme.classList.toggle("bi-moon-stars");
+
+  /* Salvar o tema no localStorage*/
+  localStorage.setItem("theme", newTheme);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const savedTheme = localStorage.getItem("theme");
+  if (savedTheme) {
+    rootHtml.setAttribute("data-theme", savedTheme);
+    if (savedTheme === "light") {
+      toggleTheme.classList.remove("bi-sun");
+      toggleTheme.classList.add("bi-moon-stars");
+    } else {
+      toggleTheme.classList.remove("bi-moon-stars");
+      toggleTheme.classList.add("bi-sun");
+    }
+  }
+});
